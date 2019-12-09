debcacd00f772bad7819bbae96906ae7d1793485
Pires <baptiste.pires37@gmail.com>
2019-12-09 12:37:27 +0100

    Intégration et modifications de la partie messagerie (ihm + logique)

--- src/model/BoiteMail.java
+++ src/model/BoiteMail.java
@@ -7,4 +7,16 @@ import java.util.List;
 public class BoiteMail implements Serializable {
     private List<Message> messages = new ArrayList<Message> ();
 
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void addMessage(Message m) {
+        this.messages.add(m);
+    }
+
+    public void removeMessage(Message m) {
+        messages.remove(m);
+    }
+
 }
--- src/model/Message.java
+++ src/model/Message.java
@@ -4,21 +4,26 @@ import java.io.Serializable;
 
 public class Message implements Serializable  {
     private String contenu;
+    private String titre;
 
     public Message() {
         super();
         lu = false;
     }
 
-    public Message(String contenu) {
+    public Message(String contenu, String titre) {
         super();
         this.contenu = contenu;
+        this.titre = titre;
     }
     private boolean lu;
 
     public void lire() {
     }
 
+    public String getTitre(){
+        return titre;
+    }
     public String getContenu() {
         return contenu;
     }
